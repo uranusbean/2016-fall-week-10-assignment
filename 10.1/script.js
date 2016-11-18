163b01c31f82807599df2daafee3c637641d3d5e
Yixuan Zhang <janicezhang.me@gmail.com>
2016-11-18 00:52:32 +0000

    done assignment 10

--- 10.1/script.js
+++ 10.1/script.js
@@ -1,4 +1,4 @@
-console.log('10.1');
+console.log('assignment 10');
 
 var m = {t:50,r:50,b:50,l:50},
     w = document.getElementById('canvas').clientWidth - m.l - m.r,
@@ -13,12 +13,27 @@ var plot = d3.select('.canvas')
 
 //Mapping specific functions
 //Projection
+var projection = d3.geoMercator()
+    .translate([w/2, h/2])
+    .scale((w - 3) / (2 * Math.PI));
 
 //Geopath
-
+var pathGenerator = d3.geoPath()
+    .projection(projection);
+    
 d3.json('../data/world-50m.json',dataloaded);
 
 function dataloaded(err, data){
     console.log(data); //This is a Topojson data
     console.log(topojson.feature(data,data.objects.countries)); //This is the converted GeoJSON data of countries
+    var countries = topojson.feature(data, data.objects.countries).features;
+    
+    projection
+        .fitExtent([[0,0],[w,h]],data);  
+    plot.selectAll(".country")  
+        .data(countries)
+        .enter().insert("path", ".graticule")
+        .attr("d", pathGenerator);
+      
+
 }
